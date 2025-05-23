d5da06ad071156596d1563b4cd33191fd1bae7c0
hode2002 <hovande.dev.2002@gmail.com>
2025-05-23 21:43:03 +0700

    Add additional image domains to next.config.ts for enhanced image optimization; include black-forest-labs-flux-1-dev.hf.space and nihalgazi-flux-pro-unlimited.hf.space to allow image loading from new sources, improving flexibility in image handling.

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,16 @@ const nextConfig: NextConfig = {
                 hostname: 'res.cloudinary.com',
                 pathname: '/**',
             },
+            {
+                protocol: 'https',
+                hostname: 'black-forest-labs-flux-1-dev.hf.space',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'nihalgazi-flux-pro-unlimited.hf.space',
+                pathname: '/**',
+            },
         ],
         deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
         imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
