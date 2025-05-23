3b38b560c080b3cd35d8a9b7593864f7e2c940f6
hode2002 <hovande.dev.2002@gmail.com>
2025-05-23 21:43:58 +0700

    Enhance HTTP response types by adding GenerateResponse and RemoveBackgroundResponse types for improved clarity and type safety in API responses related to image generation and background removal.

--- src/types/http.type.ts
+++ src/types/http.type.ts
@@ -1,6 +1,20 @@
 export type BaseResponse<T> = {
-  status: string;
-  statusCode: number;
-  message: string;
-  data: T;
+    status: string;
+    statusCode: number;
+    message: string;
+    data: T;
+};
+
+export type GenerateResponse = {
+    success: boolean;
+    data: {
+        generatedImageUrl: string;
+    };
+};
+
+export type RemoveBackgroundResponse = {
+    success: boolean;
+    data: {
+        processedImageUrl: string;
+    };
 };
