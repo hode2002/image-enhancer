1706d7deaf5aa7c72da655e8928cb5b50a1d76e4
hode2002 <hovande.dev.2002@gmail.com>
2025-05-24 19:53:40 +0700

    Enhance Home page with RotatingText component for animated text transitions; improve visual appeal by integrating customizable text rotation features within the main heading for a more engaging user experience.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,29 @@ import { GenerateForm } from '@/components/GenerateForm';
 import { Button } from '@/components/ui/button';
 import { ArrowRight } from 'lucide-react';
 import Link from 'next/link';
+import RotatingText from '@/components/blocks/TextAnimations/RotatingText/RotatingText';
 
 export default function Home() {
     return (
         <div className="flex min-h-screen flex-col py-8">
             <section className="flex flex-col items-center justify-center space-y-4 px-4 py-24 text-center">
                 <h1 className="text-4xl font-bold tracking-tighter sm:text-5xl md:text-6xl lg:text-7xl">
-                    Transform Your Images with{' '}
+                    <span className="flex gap-2">
+                        <RotatingText
+                            texts={['Generate', 'Transform']}
+                            mainClassName="w-fit px-2 sm:px-2 md:px-3 bg-gradient-to-l from-blue-500 to-purple-600 bg-cyan-300 text-black overflow-hidden py-0.5 sm:py-1 md:py-2 justify-center rounded-lg"
+                            staggerFrom={'last'}
+                            initial={{ y: '100%' }}
+                            animate={{ y: 0 }}
+                            exit={{ y: '-120%' }}
+                            staggerDuration={0.025}
+                            splitLevelClassName="overflow-hidden pb-0.5 sm:pb-1 md:pb-1"
+                            transition={{ type: 'spring', damping: 30, stiffness: 400 }}
+                            rotationInterval={2000}
+                        />
+                        <span>Your Images with</span>{' '}
+                    </span>
+
                     <span className="bg-gradient-to-r from-blue-500 to-purple-600 bg-clip-text text-transparent">
                         AI-Powered Magic
                     </span>
