b6fe5986f4dea9fe9b7317dfe68fb3491965169a
hode2002 <hovande.dev.2002@gmail.com>
2025-05-26 16:23:34 +0700

    Refactor Home component layout to improve responsiveness; adjust heading structure and spacing for better visual alignment and user experience.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,11 +8,11 @@ export default function Home() {
     return (
         <div className="flex min-h-screen flex-col py-8">
             <section className="flex flex-col items-center justify-center space-y-4 px-4 py-24 text-center">
-                <h1 className="text-4xl font-bold tracking-tighter sm:text-5xl md:text-6xl lg:text-7xl">
-                    <span className="flex gap-2">
+                <h1 className="text-4xl font-bold tracking-tighter md:text-4xl lg:text-7xl">
+                    <span className="flex flex-col gap-2 md:flex-row">
                         <RotatingText
                             texts={['Generate', 'Transform']}
-                            mainClassName="w-fit px-2 sm:px-2 md:px-3 bg-gradient-to-l from-blue-500 to-purple-600 bg-cyan-300 text-black overflow-hidden py-0.5 sm:py-1 md:py-2 justify-center rounded-lg"
+                            mainClassName="w-full md:w-fit px-2 sm:px-2 md:px-3 bg-gradient-to-l from-blue-500 to-purple-600 bg-cyan-300 text-black overflow-hidden py-0.5 sm:py-1 md:py-2 justify-center rounded-lg"
                             staggerFrom={'last'}
                             initial={{ y: '100%' }}
                             animate={{ y: 0 }}
