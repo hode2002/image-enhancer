2171e96c71c4609fdcea0da5506b00829c7dde7a
hode2002 <hovande.dev.2002@gmail.com>
2025-05-27 22:33:26 +0700

    Implement CORS configuration in main.ts to enhance security and control over allowed origins. The new setup utilizes environment variables for dynamic origin management and improves error handling with ForbiddenException for disallowed origins.

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import { ForbiddenException } from '@nestjs/common';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
@@ -5,18 +7,32 @@ import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import helmet from 'helmet';
 
+const allowedOrigins = process.env.ALLOWED_ORIGINS;
+
+const corsOptions: CorsOptions = {
+    origin: (
+        origin: string,
+        callback: (error: ForbiddenException | null, isAllow?: boolean) => void,
+    ) => {
+        if ((allowedOrigins && allowedOrigins.includes(origin)) || !origin) {
+            callback(null, true);
+        } else {
+            callback(new ForbiddenException('Origin not allowed by CORS'));
+        }
+    },
+    methods: 'GET,POST,PUT,PATCH,DELETE',
+    credentials: true,
+};
+
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     const configService = app.get(ConfigService);
 
+    app.enableCors(corsOptions);
+
     app.use(helmet());
     app.use(compression());
     app.use(cookieParser());
-
-    app.enableCors({
-        origin: configService.get<string>('FRONTEND_URL') || 'http://localhost:3000',
-        credentials: true,
-    });
 
     app.setGlobalPrefix('api/v1');
 
